84309e050a505bf596c65e881924a551038cd9fd
Eric Chanudet <chanudete@ainfosec.com>
2017-03-16 15:03:44 -0400

    input: Remove header noise.
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- util.h
+++ util.h
@@ -1,10 +1,4 @@
 /*
- * util.h:
- *
- *
- */
-
-/*
  * Copyright (c) 2012 Citrix Systems, Inc.
  * 
  * This library is free software; you can redistribute it and/or
@@ -20,15 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-
-/*
- * $Id:$
- */
-
-/*
- * $Log:$
  */
 
 #ifndef __UTIL_H__
